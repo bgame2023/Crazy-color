a806818ed9a4b76fceac6799f72d8b79a553d44a
bgame2023 <hoanghuyna.game@gmail.com>
2023-12-13 14:11:09 +0700

    source
    gitgnore

---
+++ Crazy Color/Assets/Scripts/Extension/Constants.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+    public static string key_best_medium = "bm";
+    public static string key_best_hard = "bh";
+
+    public static string sceneMain = "MainMenu";
+}
